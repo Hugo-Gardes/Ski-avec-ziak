7c88e7a5a17e7154fa8cb41ac190b0cf9947bec1
Mth3430 <mathieu.pernot@epitech.eu>
2023-03-25 17:49:50 +0100

    added menu

---
+++ ski-avec-ziak/Assets/Menu.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+/*    public *//*all_var_script*//* all_var;
+*/
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MenuGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+/*    public void return_menu()
+    {
+        if (!all_var)
+        {
+            all_var = GameObject.Find("all_var").GetComponent<all_var_script>();
+        }
+        all_var.Reset();
+        SceneManager.LoadScene(0);
+    }*/
+}
