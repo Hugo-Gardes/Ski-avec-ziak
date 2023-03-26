e2a536386883c06edb583054ac359419309356aa
Hugo-Gardes <hugo.gardes@epitech.eu>
2023-03-26 17:07:35 +0200

    ski movement + get para

---
+++ ski-avec-ziak/Assets/scripts/main.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape)) {
+            Application.Quit();
+        }
+    }
+}
